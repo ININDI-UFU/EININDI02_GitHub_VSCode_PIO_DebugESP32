de1d1a32362f3f91adbae6ce131d4e6bfa8155a3
alunoUFU <alunoufu@ufu.br>
2025-11-03 15:03:03 -0300

    refactor iikit

--- src/esp0.cpp
+++ src/esp0.cpp
@@ -1,21 +1,21 @@
-#include <Arduino.h>
-#define def_pin_D1 02
-#define pinANALOG 36  
-
-void printSerial(const uint32_t &currentMilis) {
-  Serial.print(">graf:");
-  Serial.print(currentMilis);
-  Serial.print(":");
-  Serial.print(analogRead(pinANALOG));
-  Serial.println("|g");
-}
+#include "iikit.h" 
 
 void blinkLEDFunc(uint8_t pin) {
   digitalWrite(pin, !digitalRead(pin));
 }
 
+//Função que le os valores dos POT e das Entradas 4 a 20 mA e plota no display
+void managerInputFunc(void) {
+    const uint16_t vlPOT1 = IIKit.analogReadPot1();
+    const uint16_t vlPOT2 = IIKit.analogReadPot2();
+    IIKit.disp.setText(2, ("P1:" + String(vlPOT1)).c_str());
+    IIKit.disp.setText(3, ("P2:" + String(vlPOT2)).c_str());    
+    IIKit.WSerial.plot("vlPOT1", vlPOT1);
+    IIKit.WSerial.plot("vlPOT2", vlPOT2);
+}
+
 void setup(){
-  Serial.begin(9600);
+  IIKit.setup();
   pinMode(def_pin_D1, OUTPUT);
 }
 
@@ -27,6 +27,7 @@ uint32_t previousTimePrintMS = 0;
 
 void loop()
 {
+  IIKit.loop();
   uint32_t currentMilis = millis();
   if ((currentMilis - previousTimeBlinkMS) >= TIME_DELAY_BLINK_MS)
   {
@@ -36,6 +37,6 @@ void loop()
   if ((currentMilis - previousTimePrintMS) >= TIME_DELAY_PRINT_MS)
   {
     previousTimePrintMS = currentMilis;
-    printSerial(currentMilis);
+    managerInputFunc();
   }  
 }
--- src/esp1.cpp
+++ src/esp1.cpp
@@ -5,9 +5,14 @@ void blinkLEDFunc(uint8_t pin) {
   digitalWrite(pin, !digitalRead(pin));
 }
 
+//Função que le os valores dos POT e das Entradas 4 a 20 mA e plota no display
 void managerInputFunc(void) {
-  IIKit.disp.setText(2, "P1:");
-  IIKit.disp.setText(3, "T1:");
+    const uint16_t vlPOT1 = IIKit.analogReadPot1();
+    const uint16_t vlPOT2 = IIKit.analogReadPot2();
+    IIKit.disp.setText(2, ("P1:" + String(vlPOT1)).c_str());
+    IIKit.disp.setText(3, ("P2:" + String(vlPOT2)).c_str());    
+    IIKit.WSerial.plot("vlPOT1", vlPOT1);
+    IIKit.WSerial.plot("vlPOT2", vlPOT2);
 }
 
 void setup()
